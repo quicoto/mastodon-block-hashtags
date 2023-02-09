14165ecd596da441b811319bdd3c0a3117806c47
Ricard Torres <ricard@ricard.dev>
2023-02-09 08:41:44 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 # Insert the URL to your instance
-serverURL='https://ricard.social'
+serverURL='https://mstdn.thms.uk'
 # Enable or disable multi user mode: 
 #  - If set to `true`, the script will not block any users that are followed by ANYONE on your instance
 #    This requires a token with `admin:write:accounts`, and `admin:read:accounts` access.
 #  - If set to `false`, the script will only exempt users from blocking that you personally are following
-#    This requires a token with `read:account` and `admin:write:accounts` access.
-multiUserMode=false
+#    This requires a token with `read:accounts` and `admin:write:accounts` access.
+multiUserMode=true
 # The tags you want to block
 tags=(
     'boudoir'
